8a444a66bcbf600acc015289c19570e01892ad33
CouldBeFree <propokerstar90@gmail.com>
2019-04-04 22:21:47 +0300

    Added new method

--- additional.js
+++ additional.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const db = mongoose.connect('mongodb://paito:newbase123@ds121652.mlab.com:21652/paito', { useNewUrlParser: true });
+mongoose.connect('mongodb://paito:newbase123@ds121652.mlab.com:21652/paito', { useNewUrlParser: true });
+const db = mongoose.connection;
 
 const getCollection = (collection) => {
-    db.users.find()
-        .exec((err, contacts) => {
-            // assert.equal(null, err);
-            console.info(contacts);
-            console.info(`${contacts.length} matches`);
-            db.close();
-        })
+    let data = collection.toString();
+    console.log(typeof data);
+    db.collection(data).find({}).exec((err, data) => {
+        console.info(data);
+        console.info(`${data.length} matches`);
+        db.close();
+    });
 };
 
 module.exports = {
